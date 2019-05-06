0a2ae085a15989c728748a5f6bd0d33de84ec396
Alisonnnmichella <alisonnnmichella@gmail.com>
2019-05-06 12:22:14 -0300

    Separo queries por punto coma

--- PracticaNorthwindIntermedio.sql
+++ PracticaNorthwindIntermedio.sql
@@ -1,6 +1,5 @@
 
-select * from Products
-go
+select * from Products;
 --20
 --For this problem, we’d like to see the total number of products in each category. Sort the results by the
 --total number of products, in descending order.
@@ -8,15 +7,13 @@ select c.CategoryName,count(p.CategoryID) as TotalProducts
 from Categories as c inner join Products as p
 on c.CategoryID=p.CategoryID
 group by c.CategoryName
-order by TotalProducts desc
-go
+order by TotalProducts desc;
 --21
 --In the Customers table, show the total number of customers per Country and City.
 
 select c.Country,c.City, count(c.customerID) as TotalCustomers
 from Customers as c
-group by c.Country,c.City
-go
+group by c.Country,c.City;
 --22
 --What products do we have in our inventory that should be reordered? For now, just use the fields
 --UnitsInStock and ReorderLevel, where UnitsInStock is less than the ReorderLevel, ignoring the fields
@@ -25,8 +22,7 @@ go
 select p.ProductID,p.ProductName,p.UnitsInStock,p.ReorderLevel
 from Products as p 
 where p.UnitsInStock<p.ReorderLevel
-order by p.ProductID
-go
+order by p.ProductID;
 --23
 --Now we need to incorporate these fields—UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued—
 --into our calculation. We’ll define “products that need reordering” with the following:
@@ -37,8 +33,7 @@ go
 select p.ProductID, p.ProductName,p.UnitsInStock,p.UnitsOnOrder,p.ReorderLevel,p.Discontinued
 from Products as p
 where p.UnitsInStock+p.UnitsOnOrder<=p.ReorderLevel
-and p.Discontinued=0
-go
+and p.Discontinued=0;
 
 --24
 --A salesperson for Northwind is going on a business trip to visit customers, and would like to see a list of
@@ -51,8 +46,7 @@ select c.CustomerID,c.CompanyName,c.Region
 from Customers as c
 order by  
 	(case when Region is not null   then 0 else 1 end),
-	c.Region,c.CustomerID
-go
+	c.Region,c.CustomerID;
 
 --25
 --Some of the countries we ship to have very high freight charges. We'd like to investigate some more
@@ -61,8 +55,7 @@ go
 select top 3 o.ShipCountry, avg(o.Freight)
 from  Orders as o
 group by o.ShipCountry
-order by avg(Freight) desc
-go
+order by avg(Freight) desc;
 
 --26/27
 --We're continuing on the question above on high freight charges. Now, instead of using all the orders we
@@ -73,7 +66,8 @@ select o.ShipCountry,avg(o.Freight) as AverageFreigth
 from  Orders as o
 where year(o.OrderDate)=1997
 group by o.ShipCountry
-order by avg(o.Freight) desc
+order by avg(o.Freight) desc;
+
 --28
 --We're continuing to work on high freight charges. We now want to get the three ship countries with the
 --highest average freight charges. But instead of filtering for a particular year, we want to use the last 12
@@ -84,8 +78,7 @@ select top 3 o.ShipCountry,avg(o.Freight) as AverageFreigth
 from  Orders as o
 where o.OrderDate >= DATEADD(month, -12, (select max(OrderDate) from Orders))
 group by o.ShipCountry
-order by avg(o.Freight) desc
-go
+order by avg(o.Freight) desc;
 
 --29
 --We're doing inventory, and need to show information like the below, for all orders. Sort by OrderID and
@@ -99,11 +92,10 @@ inner join Employees as e
 on o.EmployeeID=e.EmployeeID 
 inner join Products as p
 on od.ProductID=p.ProductID
-order by o.OrderID,od.ProductID
-go
+order by o.OrderID,od.ProductID;
 
 --30
---There are some customers who have never actually placed an order. Show these customers.select * from Customerswhere CustomerID not in (select CustomerID from Orders)goselect c.CustomerID as Customers_CustomerID, o.CustomerID as Orders_CustomerID from Customers as c left  join Orders as oon c.CustomerID=o.CustomerIDwhere o.CustomerID is nullgo
+--There are some customers who have never actually placed an order. Show these customers.select * from Customerswhere CustomerID not in (select CustomerID from Orders);select c.CustomerID as Customers_CustomerID, o.CustomerID as Orders_CustomerID from Customers as c left  join Orders as oon c.CustomerID=o.CustomerIDwhere o.CustomerID is null;
 
 --31
 --One employee (Margaret Peacock, EmployeeID 4) has placed the most orders. However, there are some
@@ -112,15 +104,13 @@ go
 
 select CustomerID
 from Customers 
-where CustomerID not in (select CustomerID from Orders where EmployeeId=4 )
-go
+where CustomerID not in (select CustomerID from Orders where EmployeeId=4 );
 
 select c.CustomerID, o.CustomerID
 from Customers as c left join Orders as o
 on (c.CustomerID=o.CustomerID
 	and o.EmployeeID=4)
-where o.CustomerID is null 
-go
+where o.CustomerID is null;
 
 
 
