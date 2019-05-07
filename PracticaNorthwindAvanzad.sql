4813d54a046ccb57fd17bd5ac2a7672ba3c67740
Alisonnnmichella <alisonnnmichella@gmail.com>
2019-05-07 17:30:17 -0300

    nuevos ejercicios realizados. Hasta el 36 terminado

--- PracticaNorthwindAvanzad.sql
+++ PracticaNorthwindAvanzad.sql
@@ -49,4 +49,25 @@ group by o.CustomerID,cus.CompanyName
 having sum(ord.UnitPrice*ord.Quantity) >15000
 order by TotalWithDiscount desc;
 
+--35
+--At the end of the month, salespeople are likely to try much harder to get orders, to meet their month-end
+--quotas. Show all orders made on the last day of the month. Order by EmployeeID and OrderID--EmployeeID OrderID OrderDate
+
+select o.EmployeeID,o.OrderID,o.OrderDate
+from Orders as o
+where (o.OrderDate)=EOMONTH(o.OrderDate)
+order by o.EmployeeID,o.OrderID
+
+--36
+--The Northwind mobile app developers are testing an app that customers will use to show orders. In order
+--to make sure that even the largest orders will show up correctly on the app, they'd like some samples of
+--orders that have lots of individual line items. Show the 10 orders with the most line items, in order of total
+--line items.
+select top 10 o.OrderID, count(od.OrderID) as LineItems
+from Orders as o inner join [Order Details] as od
+on o.OrderID=od.OrderID
+group by o.OrderID
+order by LineItems desc;
+
+
 
